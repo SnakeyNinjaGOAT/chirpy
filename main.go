72bafc8add7566e30bd6172e1df4abf77a95fd52
SnakeyNinjaGOAT <daniellbow@gmail.com>
2026-04-18 21:53:00 +0100

    added middleware to track metrics

--- main.go
+++ main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"net/http"
 	"os"
-	"time"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(writer, request)
+	})
+}
 
 func main() {
 	mux := http.NewServeMux()
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("./static/"))))
-	mux.HandleFunc("/healthz", handleReadyPath)
+	apiCfg := apiConfig{}
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handleFileServer()))
+	mux.HandleFunc("GET /api/healthz", handleReadyPath)
+	mux.HandleFunc("GET /api/metrics", apiCfg.handlerTotalRequests)
+	mux.HandleFunc("POST /api/reset", apiCfg.handleMetricReset)
 
 	server := &http.Server{
 		Handler: mux,
@@ -27,8 +42,32 @@ func main() {
 	}
 }
 
+func (cfg *apiConfig) handleMetricReset(writer http.ResponseWriter, request *http.Request) {
+	oldHits := cfg.fileserverHits.Swap(0)
+	msg := fmt.Sprintf("Metrics Reset\nPrevious hits: %v", oldHits)
+	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(http.StatusOK)
+	writer.Write([]byte(msg))
+
+}
+
+func handleFileServer() http.Handler {
+	return http.StripPrefix("/app", http.FileServer(http.Dir("./static/")))
+}
+
 func handleReadyPath(writer http.ResponseWriter, request *http.Request) {
-	writer.Header().Set("Content-Type", "text/plain")
+	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(http.StatusOK)
 	writer.Write([]byte("OK"))
-	http.ServeContent(writer, request, "", time.Now(), &bytes.Reader{})
+
+}
+
+func (cfg *apiConfig) handlerTotalRequests(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+
+	writer.WriteHeader(http.StatusOK)
+	totalHits := fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())
+
+	writer.Write([]byte(totalHits))
+
 }
